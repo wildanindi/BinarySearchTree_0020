a3041a8e8e31627f99f548599e565f72ed3c58c8
wildanindi <wildanindi2@gmail.com>
2024-06-13 20:02:26 +0700

    membuat pilihan untuk switch case

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -113,5 +113,22 @@ class BinaryTree
                 cout << ptr->info << " ";
             }
         }
-
 };
+
+int main()
+{
+    BinaryTree obj;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement Insert Operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        
+    }
+}
