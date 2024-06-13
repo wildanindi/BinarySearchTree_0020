8930e304a8a8ca269da39f24a3f93d45123ca0df
wildanindi <wildanindi2@gmail.com>
2024-06-13 19:54:17 +0700

    menambah prosedur pada pengulangan if

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -85,7 +85,18 @@ class BinaryTree
 
         void preorder(Node* ptr)
         {
-            
+            if (ROOT == NULL)
+            {
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr != NULL)
+            {
+                cout << ptr->info << " ";
+                preorder(ptr->leftchild);
+                preorder(ptr->rightchild);
+            }
         }
+        
 
 };
