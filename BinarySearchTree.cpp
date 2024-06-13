c262fd5ef62a765f493a6f472530ea35160cc7d1
wildanindi <wildanindi2@gmail.com>
2024-06-13 19:42:26 +0700

    menambah method search yang berisi perulangan while

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -53,4 +53,12 @@ class BinaryTree
             }
         }
 
+        void search(string element, Node*& parent, Node*& currentNode)
+        {
+            // this function search the currentNode of the specified Node as well as the current Node of its parent
+            currentNode = ROOT;
+            parent = NULL;
+            while ((currentNode != NULL) && (currentNode->info != element))
+        }
+
 };
