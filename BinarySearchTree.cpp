fe57fef211fe04d084315c2bfa5743772d262fc6
wildanindi <wildanindi2@gmail.com>
2024-06-13 18:43:21 +0700

    masih di dalam perbandingan element dan parent

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -45,7 +45,11 @@ class BinaryTree
 
             if (element < parent->info) //if the value in the data field of the new node is less than that of the parent
             {
-
+                parent->leftchild = newNode; // make the left child of the parent point on the new node
+            }
+            else if (element > parent->info)// if the value in the data field of the new data is greater than thatof the parent
+            {
+                parent->rightchild = newNode; // Make the right child of the parent point to the new node
             }
         }
 
